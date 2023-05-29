5be7b10fd3358e7e5b69b68f89edf356b130d0b1
Samir Reyes <samir1604@gmail.com>
2023-05-29 01:51:33 -0400

    Implemented Floating Action Button and removed AppBar

--- task_three/lib/features/auth/views/home_page.dart
+++ task_three/lib/features/auth/views/home_page.dart
@@ -15,117 +15,108 @@ class HomePage extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: AppColors.backgroundColor,
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 8.0),
-            child: IconButton(
-              icon: const Icon(Icons.delete),
-              onPressed: () {
-                showDialog(
-                    context: context,
-                    builder: (_) => AlertDialog(
-                          icon: const Icon(Icons.delete),
-                          title: const Text('Clean all registered users ?'),
-                          actions: [
-                            ElevatedButton.icon(
-                                onPressed: () => Navigator.pop(context),
-                                icon: const Icon(Icons.close),
-                                label: const Text('Cancel')),
-                            ElevatedButton.icon(
-                                onPressed: () {
-                                  Provider.of<UserController>(context,
-                                          listen: false)
-                                      .clear();
-                                  Navigator.pushNamedAndRemoveUntil(
-                                      context, Routes.login, (_) => false);
-                                },
-                                icon: const Icon(Icons.check),
-                                label: const Text('Accept'))
-                          ],
-                        ));
-              },
-            ),
-          )
-        ],
-      ),
+      floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.red.shade400,
+          child: Icon(
+            Icons.delete,
+            color: Colors.yellow.shade200,
+          ),
+          onPressed: () => showDialog(
+              context: context,
+              builder: (_) => AlertDialog(
+                    icon: const Icon(Icons.delete),
+                    title: const Text('Clean all registered users ?'),
+                    actions: [
+                      ElevatedButton.icon(
+                          onPressed: () => Navigator.pop(context),
+                          icon: const Icon(Icons.close),
+                          label: const Text('Cancel')),
+                      ElevatedButton.icon(
+                          onPressed: () {
+                            Provider.of<UserController>(context, listen: false)
+                                .clear();
+                            Navigator.pushNamedAndRemoveUntil(
+                                context, Routes.login, (_) => false);
+                          },
+                          icon: const Icon(Icons.check),
+                          label: const Text('Accept'))
+                    ],
+                  ))),
       body: SafeArea(
-          child: SizedBox(
-        width: double.infinity,
-        height: sizeHeight,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            SizedBox(height: sizeHeight * .1),
-            CircleAvatar(
-              radius: 60,
-              child: Icon(
-                Icons.person,
-                size: 100,
-                color: AppColors.textColor,
+          child: SingleChildScrollView(
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(height: sizeHeight * .1),
+              CircleAvatar(
+                radius: 60,
+                child: Icon(
+                  Icons.person,
+                  size: 100,
+                  color: AppColors.textColor,
+                ),
               ),
-            ),
-            SizedBox(height: sizeHeight * .05),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Card(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: SizedBox(
-                    width: double.infinity,
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        const SizedBox(height: 15),
-                        Text(
-                          'User Profile',
-                          style: TextStyle(
-                              color: AppColors.backgroundColor,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16),
-                        ),
-                        const Divider(
-                          color: Colors.black54,
-                        ),
-                        const SizedBox(height: 10),
-                        Text(
-                          userController.getRegisteredUser?.username ?? '',
-                          style: const TextStyle(fontSize: 20),
-                        ),
-                        const SizedBox(height: 10),
-                        Text(
-                          userController.getRegisteredUser?.email ?? '',
-                          style: const TextStyle(fontSize: 16),
-                        ),
-                        const SizedBox(height: 15),
-                      ],
+              SizedBox(height: sizeHeight * .05),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          const SizedBox(height: 15),
+                          Text(
+                            'User Profile',
+                            style: TextStyle(
+                                color: AppColors.backgroundColor,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16),
+                          ),
+                          const Divider(
+                            color: Colors.black54,
+                          ),
+                          const SizedBox(height: 10),
+                          Text(
+                            userController.getRegisteredUser?.username ?? '',
+                            style: const TextStyle(fontSize: 20),
+                          ),
+                          const SizedBox(height: 10),
+                          Text(
+                            userController.getRegisteredUser?.email ?? '',
+                            style: const TextStyle(fontSize: 16),
+                          ),
+                          const SizedBox(height: 15),
+                          const Divider(),
+                          const SizedBox(height: 15),
+                          SizedBox(
+                            width: double.infinity,
+                            child: ElevatedButton.icon(
+                              onPressed: () {
+                                Provider.of<UserController>(context,
+                                        listen: false)
+                                    .logOut();
+                                Navigator.pushNamedAndRemoveUntil(
+                                    context, Routes.login, (_) => false);
+                              },
+                              icon:
+                                  const Icon(Icons.arrow_circle_left_outlined),
+                              label: const Text('Close Session'),
+                            ),
+                          ),
+                          const SizedBox(height: 5),
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            const Spacer(),
-            SizedBox(
-              width: double.infinity,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: OutlinedButton(
-                  style: const ButtonStyle(
-                      backgroundColor:
-                          MaterialStatePropertyAll<Color>(Colors.white)),
-                  onPressed: () {
-                    Provider.of<UserController>(context, listen: false)
-                        .logOut();
-                    Navigator.pushNamedAndRemoveUntil(
-                        context, Routes.login, (_) => false);
-                  },
-                  child: const Text('Close Session'),
-                ),
-              ),
-            ),
-            const SizedBox(height: 15),
-          ],
+            ],
+          ),
         ),
       )),
     );
